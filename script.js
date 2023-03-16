24e227f20f492a8fd6c19c12b1dff356bebdcd83
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-03-16 18:38:32 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+    const container = document.getElementById("container");
+
+    this.fetch('https://handlers.education.launchcode.org/static/astronauts.json').then()
+})
