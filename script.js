b97ef8a1e4f48d1a89612a803b48ed8313fe304a
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-03-16 21:23:49 -0500

    updated script.js

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(f
         response.json().then(function(data){
             console.log(data);
 for(let i=0;i<data.length; i++){
+    let astronaut = data[i];
     container.innerHTML += `
 <div class="astronaut">
    <div class="bio">
-      <h3>Mae Jemison</h3>
+      <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
       <ul>
-         <li>Hours in space: 190</li>
-         <li>Active: false</li>
-         <li>Skills: Physician, Chemical Engineer</li>
+         <li>Hours in space: ${astronaut.hoursInSpace}</li>
+         <li>Active: ${astronaut.active}</li>
+         <li>Skills: ${astronaut.skills.join(", ")}</li>
       </ul>
    </div>
-   <img class="avatar" src="images/mae-jemison.jpg">
+   <img class="avatar" src="${astronaut.picture}">
 </div>
 `
 }
