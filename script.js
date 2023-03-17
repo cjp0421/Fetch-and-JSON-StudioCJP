919f3fcbd1a62b138398e799123fd445ac3b3c00
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-03-16 19:24:31 -0500

    updated script js
    added hardcoded practice data
    added fetch and promise
    added for loop

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // TODO: add code here
 window.addEventListener('load', function(){
     const container = document.getElementById("container");
+fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+for(let i=0;i<data.length; i++){
+    container.innerHTML += `
+<div class="astronaut">
+   <div class="bio">
+      <h3>Mae Jemison</h3>
+      <ul>
+         <li>Hours in space: 190</li>
+         <li>Active: false</li>
+         <li>Skills: Physician, Chemical Engineer</li>
+      </ul>
+   </div>
+   <img class="avatar" src="images/mae-jemison.jpg">
+</div>
+`
+}
 
-    this.fetch('https://handlers.education.launchcode.org/static/astronauts.json').then()
+
+
+        })
+    })
 })
